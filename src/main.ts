6f498692c49cf08fde53db28048f3d1903e0df57
Gabriel Oliveira <gabrielferrarez77@gmai.com>
2026-05-21 19:48:41 -0300

    feat: add app module, controller and service

---
+++ src/main.ts
@@ -0,0 +1,57 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser()); 
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 8000;
+
+  app.enableCors({
+    origin: `http://localhost:3001`,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Elock API')
+    .setDescription('API para sistema de tranca IoT')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        description: 'Insira o token JWT no formato Bearer <token>',
+        in: 'header',
+      },
+      'jwt-auth',
+    )
+    .addTag('auth')
+    .addTag('users')
+    .addTag('door-locks')
+    .addTag('door-lock-user')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  await app.startAllMicroservices();
+  await app.listen(port);
+}
+bootstrap();
